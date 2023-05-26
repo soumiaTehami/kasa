bfc2b22d9530b701b72672438f10ec905b00273f
tehami soumia <sou_mia_46@hotmail.fr>
2023-05-26 20:11:58 +0200

    page apartement2

--- kasa/src/page/ApartementPage.js
+++ kasa/src/page/ApartementPage.js
@@ -35,15 +35,15 @@ function ApartementPage() {
               </div>
             </div>
             <div className="apartement-description">
-              <p className="description-title">Description</p>
-              <p className="description-content">
+              <p className="description-title"><span>Description</span><i className="fa-solid fa-chevron-up"></i></p>
+              <p className="description-content"> 
                 Votre maison loin de chez vous. Que vous veniez de l'autre bout
                 du monde, ou juste de quelques stations de RER, vous vous
                 sentirez chez vous dans notre appartement.
               </p>
             </div>
             <div className="apartement-description">
-              <p className="description-title">Description</p>
+              <p className="description-title"><span>Description</span><i className="fa-solid fa-chevron-up"></i></p>
               <p className="description-content">
                 Votre maison loin de chez vous. Que vous veniez de l'autre bout
                 du monde, ou juste de quelques stations de RER, vous vous
